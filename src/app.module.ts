108b173385d7f0f6e4c3843a0d712fb31e4aa548
Larry Borsato <larry@larryborsato.com>
2022-02-17 09:52:51 -0500

    Implement original connector features.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { RegistrationService } from './services/registration/registration.service';
+import { configuration } from './configuration'; // this is new
 
 @Module({
-  imports: [],
+  imports: [
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    // }),
+
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env`,
+      load: [configuration],
+    }),
+    HttpModule.register({
+      timeout: 30000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RegistrationService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
-      .setVersion('1.0')
-      .addTag('cats')
-      .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //     .setTitle(process.env.name)
+  //     .setDescription(process.env.name)
+  //     .setVersion(process.env.version)
+  //     .addTag('connector')
+  //     .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT);
 }
 bootstrap();
